a9867b5cce74de650f01cf6a0a9fb395303276dd
Georgina Bada <georginabada@gmail.com>
2022-01-03 22:17:18 -0500

    BootcampX Queries 3 - Group By

---
+++  3_queries_group-by/low_avg_time.sql
@@ -0,0 +1,17 @@
+
+-- Get currently enrolled students' average assignment completion time.
+
+-- Select the students name and average time from assignment submissions.
+-- Order the results from greatest duration to least greatest duration.
+-- A student will have a null end_date if they are currently enrolled
+
+
+
+SELECT students.name as student, avg(assignment_submissions.duration) as average_assignment_duration, avg(assignments.duration) as average_estimated_duration
+FROM students
+JOIN assignment_submissions ON student_id = students.id
+JOIN assignments ON assignments.id = assignment_id
+WHERE end_date IS NULL
+GROUP BY student
+HAVING avg(assignment_submissions.duration) < avg(assignments.duration)
+ORDER BY average_assignment_duration;
